a0b3f6f1b476702b463da9dc677d43f29e86f46e
chamcherno <cherno@writemedia.co.uk>
2023-05-17 15:27:32 +0000

    * Adding the GlobalUsings file in the UI project. * Adding the DataConnection file in the UI project. * Adding the SuggestionModel in the UI project.
    * Adding the GlobalUsings file in the Library project.
    * Configured dependacy injection in the RegisterServices file in the UI.
    * Adding caching( in memory caching) in the RegisterServices file.

--- SuggestionAppUI/Program.cs
+++ SuggestionAppUI/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using SuggestionAppUI;
 using SuggestionAppUI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
+builder.ConfigureServices();
 
 var app = builder.Build();
 
